6fe22babb1fd98723441dd28e4918cdb204c3e53
BryanZhu <bzhu@ambiq.com>
2023-09-28 10:52:44 +0800

    Optimized the ambt53 driver to be more configurable through DTS

--- samples/out_of_tree_driver/custom_drivers/zephyr/ambt53/ambt53_xip_driver.c
+++ samples/out_of_tree_driver/custom_drivers/zephyr/ambt53/ambt53_xip_driver.c
@@ -13,6 +13,7 @@
 #include <zephyr/drivers/spi.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/sys/byteorder.h>
+#include <zephyr/dt-bindings/flash_controller/ospi.h>
 #include "custom_mspi_ambiq.h"
 #include <am_mcu_apollo.h>
 /**
@@ -20,39 +21,37 @@
  * implementation. See the header file of the same name for details.
  */
 struct ambt53_config {
-	uint32_t dqs_en;
+	bool dqs_en;
 	const struct device *bus;
 	struct gpio_dt_spec reset_gpio;
 	uint32_t spi_bus_width;
 	uint32_t max_frequency;
+	uint32_t dummy_cycles;
 };
 
-static struct ambt53_data {
+struct ambt53_data {
 	const struct device *mspi_dev;
-} xip_data;
-
-static struct spi_config spi_cfg = {
-	.operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_MODE_CPHA | SPI_WORD_SET(8),
-	.frequency = 4000000,
 };
 
 static struct mspi_buf g_mspi_buf;
-static int ambt53_remote_init(const struct device *xip_dev, am_hal_mspi_dev_config_t *pConfig, bool bDQSEn);
+static int ambt53_remote_init(const struct device *xip_dev);
 static void ambt53_remote_reset(const struct device *xip_dev);
 
 static int ambt53_init(const struct device *xip_dev)
 {
 	int ret;
-	struct ambt53_data *data = xip_dev->data;
 	const struct ambt53_config *config = xip_dev->config;
+	struct spi_config spi_cfg = {
+	.operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_MODE_CPHA | SPI_WORD_SET(8),
+	.frequency = config->max_frequency,
+	};
 
-	data->mspi_dev = config->bus;
-	if (!device_is_ready(data->mspi_dev)) {
+	if (!device_is_ready(config->bus)) {
 		printk("mspi_dev is not ready\n");
 		return -ENODEV;
 	}
 
-	ret = custom_mspi_config(data->mspi_dev, &spi_cfg);
+	ret = custom_mspi_config(config->bus, &spi_cfg);
 	if (ret < 0) {
 		printk("SPI configure error: %d\n", ret);
 	}
@@ -64,11 +63,11 @@ static int ambt53_init(const struct device *xip_dev)
 	g_mspi_buf.send_addr = 1;
 	g_mspi_buf.instr = 0xAB;
 	g_mspi_buf.addr = 0x88776655;
-	ret = custom_mspi_write(data->mspi_dev, &g_mspi_buf);
+	ret = custom_mspi_write(config->bus, &g_mspi_buf);
 	if (ret < 0) {
 		printk("SPI write error: %d\n", ret);
 	}
-	ret = custom_mspi_read(data->mspi_dev, &g_mspi_buf);
+	ret = custom_mspi_read(config->bus, &g_mspi_buf);
 	if (ret < 0) {
 		printk("SPI read error: %d\n", ret);
 	}
@@ -78,18 +77,33 @@ static int ambt53_init(const struct device *xip_dev)
 	}
 
 	ambt53_remote_reset(xip_dev);
-	struct mspi_ambiq_data *mspi_dev_data = (struct mspi_ambiq_data *)data->mspi_dev->data;
-	ambt53_remote_init(xip_dev, &mspi_dev_data->mspi_dev_cfg, false);
-
-	// Switch MSPI to Quad mode
-	spi_cfg.operation &= (~SPI_LINES_MASK);
-	spi_cfg.operation |= SPI_LINES_QUAD;
-	printk("QSPI configure =: 0x%x\n", spi_cfg.operation);
-	ret = custom_mspi_config(data->mspi_dev, &spi_cfg);
+	ambt53_remote_init(xip_dev);
+	// Switch MSPI to Quad/Octol mode if device spi width is 
+	if(config->spi_bus_width != OSPI_SPI_MODE)
+	{
+		spi_cfg.operation &= (~SPI_LINES_MASK);
+		switch (config->spi_bus_width)
+		{
+		case OSPI_DUAL_MODE:
+			spi_cfg.operation |= SPI_LINES_DUAL;
+			break;
+		case OSPI_QUAD_MODE:
+			spi_cfg.operation |= SPI_LINES_QUAD;
+			break;
+		case OSPI_OPI_MODE:
+			spi_cfg.operation |= SPI_LINES_OCTAL;
+			break;
+		}
+		printk("QSPI configure =: 0x%x\n", spi_cfg.operation);
+		
+	}
+	
+	ret = custom_mspi_config(config->bus, &spi_cfg);
 	if (ret < 0) {
 		printk("QSPI configure error: %d\n", ret);
 	}
-	ret = custom_mspi_enable_xip(data->mspi_dev);
+
+	ret = custom_mspi_enable_xip(config->bus);
 	if (ret < 0) {
 		printk("Enable XIP error: %d\n", ret);
 	}
@@ -109,7 +123,7 @@ static int ambt53_init(const struct device *xip_dev)
 //*****************************************************************************
 static void ambt53_basic_config_get(const struct device *xip_dev)
 {
-	struct ambt53_data *data = xip_dev->data;
+	const struct ambt53_config *config = xip_dev->config;
     uint32_t ui32Ver = 0;
     uint32_t ui32Cfg = 0;
     ambt53_status_reg_t stStatus = {0};
@@ -118,11 +132,11 @@ static void ambt53_basic_config_get(const struct device *xip_dev)
 	g_mspi_buf.buf = &ui32Ver;
 	g_mspi_buf.len = sizeof(ui32Ver);
 	g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_READ_VER;
-    custom_mspi_read(data->mspi_dev, &g_mspi_buf);
+    custom_mspi_read(config->bus, &g_mspi_buf);
 	g_mspi_buf.buf = &ui32Cfg;
 	g_mspi_buf.len = sizeof(ui32Cfg);
 	g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_READ_CFG;
-    custom_mspi_read(data->mspi_dev, &g_mspi_buf);
+    custom_mspi_read(config->bus, &g_mspi_buf);
     if ( (ui32Ver == 0) || (ui32Cfg == 0) )
     {
         printk("The xSPI IP is not working! Please check your hardware...\n");
@@ -143,13 +157,13 @@ static void ambt53_basic_config_get(const struct device *xip_dev)
 //*****************************************************************************
 void ambt53_status_get(const struct device *xip_dev, ambt53_status_reg_t *pStatus)
 {
-	struct ambt53_data *data = xip_dev->data;
+	const struct ambt53_config *config = xip_dev->config;
 	g_mspi_buf.send_addr = 0;
 	g_mspi_buf.addr = 0;
 	g_mspi_buf.buf = pStatus;
 	g_mspi_buf.len = sizeof(ambt53_status_reg_t);
 	g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_READ_STS;
-    custom_mspi_read(data->mspi_dev, &g_mspi_buf);
+    custom_mspi_read(config->bus, &g_mspi_buf);
     pStatus->STATUS = __bswap_32(pStatus->STATUS);
 }
 
@@ -161,13 +175,13 @@ void ambt53_status_get(const struct device *xip_dev, ambt53_status_reg_t *pStatu
 void ambt53_status_clear(const struct device *xip_dev, ambt53_status_reg_t *pStatus)
 {
     pStatus->STATUS = __bswap_32(pStatus->STATUS);
-	struct ambt53_data *data = xip_dev->data;
+	const struct ambt53_config *config = xip_dev->config;
 	g_mspi_buf.send_addr = 0;
 	g_mspi_buf.addr = 0;
 	g_mspi_buf.buf = pStatus;
 	g_mspi_buf.len = sizeof(ambt53_status_reg_t);
 	g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_WRITE_STS;
-    custom_mspi_write(data->mspi_dev, &g_mspi_buf);
+    custom_mspi_write(config->bus, &g_mspi_buf);
 }
 
 //*****************************************************************************
@@ -178,13 +192,13 @@ void ambt53_status_clear(const struct device *xip_dev, ambt53_status_reg_t *pSta
 static void ambt53_remote_reset(const struct device *xip_dev)
 {
     //Reset to default
-	struct ambt53_data *data = xip_dev->data;
+	const struct ambt53_config *config = xip_dev->config;
 	g_mspi_buf.send_addr = 0;
 	g_mspi_buf.addr = 0;
 	g_mspi_buf.buf = NULL;
 	g_mspi_buf.len = 0;
 	g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOFT_RESET;
-    custom_mspi_write(data->mspi_dev, &g_mspi_buf);
+    custom_mspi_write(config->bus, &g_mspi_buf);
 	// Delay at least 5 SPI cycles
 	k_busy_wait(1);
 }
@@ -194,79 +208,43 @@ static void ambt53_remote_reset(const struct device *xip_dev)
 //  Initialize the MSPI remote driver.
 //
 //*****************************************************************************
-static int ambt53_remote_init(const struct device *xip_dev, am_hal_mspi_dev_config_t *pConfig, bool bDQSEn)
+static int ambt53_remote_init(const struct device *xip_dev)
 {
-    struct ambt53_data *data = xip_dev->data;
+    const struct ambt53_config *config = xip_dev->config;
 	g_mspi_buf.send_addr = 0;
 	g_mspi_buf.addr = 0;
 	g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SET_DUMMY;
-    
-    if ( !bDQSEn )
+    // Dummy cycle takes effective only if DQS mode is disabled
+    if ( !config->dqs_en )
     {
-        uint32_t DummyBytes = 0;
-        if ( !pConfig->bTurnaround )
-        {
-            DummyBytes = 0;
-        }
-        else
-        {
-            // Dummy should between 4-255
-            if ( pConfig->ui8TurnAround >= 4 )
-            {
-                DummyBytes = pConfig->ui8TurnAround;
-            }
-            else
-            {
-                return -EINVAL;
-            }
-        }
+        uint32_t dummy_cycles = 0;
+		// Dummy should between 4-255
+		if ( config->dummy_cycles >= 4 )
+		{
+			dummy_cycles = config->dummy_cycles;
+		}
+		else
+		{
+			return -EINVAL;
+		}
 
 		g_mspi_buf.len = 1;
-		g_mspi_buf.buf = &DummyBytes;
-		custom_mspi_write(data->mspi_dev, &g_mspi_buf);
+		g_mspi_buf.buf = &dummy_cycles;
+		custom_mspi_write(config->bus, &g_mspi_buf);
     }
 
 	g_mspi_buf.len = 0;
 	g_mspi_buf.buf = NULL;
-    switch ( pConfig->eDeviceConfig )
+    switch ( config->spi_bus_width )
     {
-        case AM_HAL_MSPI_FLASH_SERIAL_CE0:
-        case AM_HAL_MSPI_FLASH_SERIAL_CE1:
+        case OSPI_SPI_MODE:
 			g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_1_1_1;
             break;
-        case AM_HAL_MSPI_FLASH_DUAL_CE0:
-        case AM_HAL_MSPI_FLASH_DUAL_CE1:
-			g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_2_2_2;
-            break;
-        case AM_HAL_MSPI_FLASH_QUAD_CE0:
-        case AM_HAL_MSPI_FLASH_QUAD_CE1:
-            if ( bDQSEn )
-            {
-				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_4_4_4_DQS;
-            }
-            else
-            {
-				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_4_4_4;
-            }
-            break;
-        case AM_HAL_MSPI_FLASH_OCTAL_CE0:
-        case AM_HAL_MSPI_FLASH_OCTAL_CE1:
-            if ( bDQSEn )
-            {
-				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_8_8_8_DQS;
-            }
-            else
-            {
-				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_8_8_8;
-            }
-            break;
-        case AM_HAL_MSPI_FLASH_DUAL_CE0_1_2_2:
-        case AM_HAL_MSPI_FLASH_DUAL_CE1_1_2_2:
+        case OSPI_DUAL_MODE:
 			g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_1_2_2;
             break;
-        case AM_HAL_MSPI_FLASH_QUAD_CE0_1_4_4:
-        case AM_HAL_MSPI_FLASH_QUAD_CE1_1_4_4:
-            if ( bDQSEn )
+        case OSPI_QUAD_MODE:
+            if ( config->dqs_en )
             {
 				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_1_4_4_DQS;
             }
@@ -275,11 +253,21 @@ static int ambt53_remote_init(const struct device *xip_dev, am_hal_mspi_dev_conf
 				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_1_4_4;
             }
             break;
+        case OSPI_OPI_MODE:
+            if ( config->dqs_en )
+            {
+				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_1_8_8_DQS;
+            }
+            else
+            {
+				g_mspi_buf.instr = AM_DEVICES_MSPI_AMBT53_SOPM_1_8_8;
+            }
+            break;
         default:
             return -ENOTSUP;
     }
 
-    custom_mspi_write(data->mspi_dev, &g_mspi_buf);
+    custom_mspi_write(config->bus, &g_mspi_buf);
     return 0;
 }
 
@@ -287,17 +275,13 @@ static struct ambt53_xip_driver_api ambt53_xip_driver_api = {
 	.print = NULL
 	};
 
-// DEVICE_DEFINE(ambt53_xip, "AMBT53_XIP_DRIVER",
-// 		    init, NULL, &xip_data, NULL,
-// 		    APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
-// 		    &ambt53_xip_driver_api);
-
 #define AMBT53_INIT(index)                                                     \
 	static const struct ambt53_config ambt53_config_##index = {	       \
 		.bus = DEVICE_DT_GET(DT_INST_BUS(index)),			       \
 		.reset_gpio = GPIO_DT_SPEC_INST_GET_OR(index, reset_gpios, {0}),  \
 		.max_frequency = DT_INST_PROP(index, max_frequency),  \
 		.spi_bus_width = DT_INST_PROP(index, spi_bus_width),  \
+		.dummy_cycles =  DT_INST_PROP(index, dummy_cycles),		       \
 		.dqs_en =  DT_INST_PROP(index, dqs_en),		       \
 	};								       \
 	static struct ambt53_data ambt53_data_##index;			       \
